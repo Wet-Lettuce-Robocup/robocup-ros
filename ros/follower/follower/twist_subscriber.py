7ddc6e53a019a3e48cf35774a4b161252dd2f1c2
William D'Olier <56065795+will-da-pro@users.noreply.github.com>
2026-01-26 22:39:49 +1100

    Require all code to pass ros2 tests before merginf
    * Change Docker image tag and add ROS testing
    
    Updated Docker image tag and added ROS test step.
    
    * Change Docker image from maze/ros to wetlettuce/robot
    
    * remove tty
    
    * Remove docker stuff and use dedicated ros2 tests instead
    
    * Update Docker workflow to use new ROS 2 distribution
    
    * Update target ROS2 distribution to 'kilted'
    
    * modified dockerfile to allow ros2 to communicate over local network (#3) (#5)
    
    * tests pass now locally

--- ros/follower/follower/twist_subscriber.py
+++ ros/follower/follower/twist_subscriber.py
@@ -1,19 +1,20 @@
-import rclpy
-from rclpy.node import Node
+import math
 
 from geometry_msgs.msg import Twist
-
-import math
+import rclpy
+from rclpy.node import Node
 import serial
 
+
 class TwistSubscriber(Node):
+
     START_FLAG: bytes = b'\xA5'
 
     DRIVE_REQUEST: bytes = b'\x30'
     STOP_REQUEST: bytes = b'\x31'
 
     def __init__(self):
-        super().__init__("twist_subscriber")
+        super().__init__('twist_subscriber')
         self.subscription = self.create_subscription(
                 Twist,
                 '/cmd_vel',
@@ -21,15 +22,15 @@ class TwistSubscriber(Node):
                 10
         )
 
-        self.ser = serial.Serial("/dev/ttyAMA0", 115200)
+        self.ser = serial.Serial('/dev/ttyAMA0', 115200)
 
-        self.wheel_dist = 0.185 # m
+        self.wheel_dist = 0.185  # m
         self.counts_per_revolution = 480
         self.max_counts_per_second = 900
         self.wheel_radius = 0.04
         self.speed_mult = self.counts_per_revolution / (self.wheel_radius * 2 * math.pi)
 
-        self.get_logger().info("Twist subscriber node started!")
+        self.get_logger().info('Twist subscriber node started!')
 
     def stop(self):
         self.ser.write(self.START_FLAG + self.STOP_REQUEST)
@@ -46,14 +47,16 @@ class TwistSubscriber(Node):
         linear_y = fitted(linear_y)
         angular_z = fitted(angular_z)
 
-        linear_x_byte: bytes = int(linear_x).to_bytes(1, 'big', signed=True) 
-        linear_y_byte: bytes = int(linear_y).to_bytes(1, 'big', signed=True) 
-        angular_z_byte: bytes = int(angular_z).to_bytes(1, 'big', signed=True) 
+        linear_x_byte: bytes = int(linear_x).to_bytes(1, 'big', signed=True)
+        linear_y_byte: bytes = int(linear_y).to_bytes(1, 'big', signed=True)
+        angular_z_byte: bytes = int(angular_z).to_bytes(1, 'big', signed=True)
 
-        self.ser.write(self.START_FLAG + self.DRIVE_REQUEST + linear_x_byte + linear_y_byte + angular_z_byte)
+        self.ser.write(self.START_FLAG + self.DRIVE_REQUEST + linear_x_byte +
+                       linear_y_byte + angular_z_byte)
 
     def __del__(self):
         self.stop()
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -62,6 +65,6 @@ def main(args=None):
     twist_subscriber_node.destroy_node()
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
     main()
-
