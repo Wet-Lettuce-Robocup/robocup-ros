d4df9110944399b13cb0cca39cdb0578b4933556
William D'Olier <william@dolier.net>
2026-04-24 13:03:08 +1000

    changed twist and odom nodes to work with i2c

--- ros/robot_core/robot_core/odom_publisher.py
+++ ros/robot_core/robot_core/odom_publisher.py
@@ -1,25 +1,31 @@
 import math
 
-# from builtin_interfaces.msg import Time
 from geometry_msgs.msg import Point, Quaternion, Twist, Vector3
 from nav_msgs.msg import Odometry
 import rclpy
 from rclpy.node import Node
-import serial
+from smbus import SMBus
 
 
 class OdomPublisher(Node):
-
-    START_FLAG: bytes = b'\xA5'
-
-    ENCODER_REQUEST: bytes = b'\x40'
-    ENCODER_RESPONSE: bytes = b'\x41'
+    ENCODER_REQUEST: bytes = b"\x40"
+    ENCODER_RESPONSE: bytes = b"\x41"
 
     def __init__(self):
-        super().__init__('odom_publisher')
-        self.publisher_ = self.create_publisher(Odometry, 'odom', 10)
+        super().__init__("odom_publisher")
+
+        self.declare_parameter("wheel_dist", 0.175)
+        self.declare_parameter("counts_per_revolution", 480.0)
+        self.declare_parameter("wheel_radius", 0.04)
+
+        self.publisher_ = self.create_publisher(Odometry, "odom", 10)
         self.timer = self.create_timer(0.1, self.publish_odom)  # 10 Hz
         self.start_time = self.get_clock().now()
+
+        self.last_enc_fl = 0.0
+        self.last_enc_fr = 0.0
+        self.last_enc_bl = 0.0
+        self.last_enc_br = 0.0
 
         self.x = 0.0
         self.y = 0.0
@@ -27,70 +33,81 @@ class OdomPublisher(Node):
         self.vx = 0.0  # m/s
         self.vth = 0.0  # rad/s
 
-        self.ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
-        self.get_logger().info(f"Serial connected: {'y' if self.ser.is_open else 'n'}")
-        self.get_logger().info(str(self.ser))
+        self.addr = 0x67
+        self.bus = SMBus(1)
 
-        self.wheel_dist = 0.185  # m
-        self.counts_per_revolution = 480
-        self.wheel_radius = 0.04
-        self.wheel_circumefrence = math.pi * (self.wheel_radius ** 2)
+        self.wheel_dist = self.get_parameter("wheel_dist").value
+        self.counts_per_revolution = self.get_parameter("counts_per_revolution").value
+        self.wheel_radius = self.get_parameter("wheel_radius").value
+
+        self.wheel_circumefrence = math.pi * (self.wheel_radius**2)
+
+    def get_vel(self):
+        vel_msg = self.bus.read_i2c_block_data(self.addr, 0x81, 16)
+
+        vel_fl = int.from_bytes(vel_msg[0:4], signed=True)
+        vel_fr = int.from_bytes(vel_msg[4:8], signed=True)
+        vel_bl = int.from_bytes(vel_msg[8:12], signed=True)
+        vel_br = int.from_bytes(vel_msg[12:16], signed=True)
+
+        return vel_fl, vel_fr, vel_bl, vel_br
 
     def get_encoders(self):
-        self.ser.write(self.START_FLAG + self.ENCODER_REQUEST)
-        msg = self.ser.read(18)
+        enc_msg = self.bus.read_i2c_block_data(self.addr, 0x82, 16)
 
-        if len(msg) < 18:
-            self.get_logger().warn(f'Wrong Size ({len(msg)})')
-            return
+        enc_fl = int.from_bytes(enc_msg[0:4], signed=True)
+        enc_fr = int.from_bytes(enc_msg[4:8], signed=True)
+        enc_bl = int.from_bytes(enc_msg[8:12], signed=True)
+        enc_br = int.from_bytes(enc_msg[12:16], signed=True)
 
-        if msg[0:1] != self.START_FLAG:
-            self.get_logger().warn(f'Incorrect Start Flag ({msg[0]})')
-            return
-
-        if msg[1:2] != self.ENCODER_RESPONSE:
-            self.get_logger().warn(f'Incorrect Response Byte ({msg[1]})')
-            return
-
-        enc_a = int.from_bytes(msg[2:6], signed=True)
-        enc_b = int.from_bytes(msg[6:10], signed=True)
-
-        speed_a = int.from_bytes(msg[10:14], signed=True)
-        speed_b = int.from_bytes(msg[14:18], signed=True)
-
-        return enc_a, enc_b, speed_a, speed_b
+        return enc_fl, enc_fr, enc_bl, enc_br
 
     def publish_odom(self):
         current_time = self.get_clock().now()
-        dt = 0.1  # seconds since last update
+        dt = 0.1
+
+        # vel_data = self.get_vel()
+
+        # if vel_data is None:
+        #     return
+
+        # vel_fl, vel_fr, vel_bl, vel_br = vel_data
 
         enc_data = self.get_encoders()
 
         if enc_data is None:
             return
 
-        enc_a, enc_b, speed_a, speed_b = enc_data
+        enc_fl, enc_fr, enc_bl, enc_br = enc_data
 
-        angular_vel_mult = 2 * math.pi * self.wheel_radius / self.counts_per_revolution
+        d_l = (enc_fl - self.last_enc_fl + enc_bl - self.last_enc_bl) / 2
+        self.last_enc_fl = enc_fl
+        self.last_enc_bl = enc_bl
 
-        vel_a = speed_a * angular_vel_mult
-        vel_b = speed_b * angular_vel_mult
+        d_r = enc_fr - self.last_enc_fr + enc_br - self.last_enc_br
+        self.last_enc_fr = enc_fr
+        self.last_enc_br = enc_br
 
-        self.vx = (vel_a + vel_b) / 2
-        self.vth = (vel_a - vel_b) / self.wheel_dist
+        angular_mult = 2 * math.pi * self.wheel_radius / self.counts_per_revolution
+
+        angle_change_l = d_l * angular_mult
+        angle_change_r = d_r * angular_mult
+
+        self.dx = (angle_change_l + angle_change_r) / 2
+        self.dth = (angle_change_l - angle_change_r) / self.wheel_dist
 
         # Update position
-        self.x += self.vx * math.cos(self.th) * dt
-        self.y += self.vx * math.sin(self.th) * dt
-        self.th += self.vth * dt
+        self.x += self.dx * math.cos(self.th)
+        self.y += self.dx * math.sin(self.th)
+        self.th += self.dth
 
         # Orientation as quaternion
         odom_quat = self.euler_to_quaternion(0, 0, self.th)
 
         odom = Odometry()
         odom.header.stamp = current_time.to_msg()
-        odom.header.frame_id = 'odom'
-        odom.child_frame_id = 'base_link'
+        odom.header.frame_id = "odom"
+        odom.child_frame_id = "base_link"
 
         # Set the position
         odom.pose.pose.position = Point(x=self.x, y=self.y, z=0.0)
@@ -101,21 +118,25 @@ class OdomPublisher(Node):
         # Set the velocity
         odom.twist.twist = Twist(
             linear=Vector3(x=self.vx, y=0.0, z=0.0),
-            angular=Vector3(x=0.0, y=0.0, z=self.vth)
+            angular=Vector3(x=0.0, y=0.0, z=self.dth / dt),
         )
 
         self.publisher_.publish(odom)
 
     def euler_to_quaternion(self, roll, pitch, yaw):
         """Convert Euler angles to quaternion."""
-        qx = math.sin(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) - \
-            math.cos(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
-        qy = math.cos(roll/2) * math.sin(pitch/2) * math.cos(yaw/2) + \
-            math.sin(roll/2) * math.cos(pitch/2) * math.sin(yaw/2)
-        qz = math.cos(roll/2) * math.cos(pitch/2) * math.sin(yaw/2) - \
-            math.sin(roll/2) * math.sin(pitch/2) * math.cos(yaw/2)
-        qw = math.cos(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) + \
-            math.sin(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
+        qx = math.sin(roll / 2) * math.cos(pitch / 2) * math.cos(yaw / 2) - math.cos(
+            roll / 2
+        ) * math.sin(pitch / 2) * math.sin(yaw / 2)
+        qy = math.cos(roll / 2) * math.sin(pitch / 2) * math.cos(yaw / 2) + math.sin(
+            roll / 2
+        ) * math.cos(pitch / 2) * math.sin(yaw / 2)
+        qz = math.cos(roll / 2) * math.cos(pitch / 2) * math.sin(yaw / 2) - math.sin(
+            roll / 2
+        ) * math.sin(pitch / 2) * math.cos(yaw / 2)
+        qw = math.cos(roll / 2) * math.cos(pitch / 2) * math.cos(yaw / 2) + math.sin(
+            roll / 2
+        ) * math.sin(pitch / 2) * math.sin(yaw / 2)
         return [qx, qy, qz, qw]
 
 
@@ -127,5 +148,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- ros/robot_core/robot_core/twist_subscriber.py
+++ ros/robot_core/robot_core/twist_subscriber.py
@@ -3,62 +3,71 @@ import math
 from geometry_msgs.msg import Twist
 import rclpy
 from rclpy.node import Node
-import serial
+from smbus import SMBus
 
 
 class TwistSubscriber(Node):
+    DRIVE_REQUEST: bytes = b"\x01"
+    STOP_REQUEST: bytes = b"\x02"
 
-    START_FLAG: bytes = b'\xA5'
+    def __init__(self):
+        super().__init__("twist_subscriber")
 
-    DRIVE_REQUEST: bytes = b'\x30'
-    STOP_REQUEST: bytes = b'\x31'
+        self.declare_parameter("wheel_dist", 0.175)
+        self.declare_parameter("counts_per_revolution", 480.0)
+        self.declare_parameter("wheel_radius", 0.04)
+        self.declare_parameter("max_counts_per_second", 900.0)
 
-    def __init__(self) -> None:
-        super().__init__('twist_subscriber')
         self.subscription = self.create_subscription(
-                Twist,
-                '/cmd_vel',
-                self.twist_callback,
-                10
+            Twist, "/cmd_vel", self.twist_callback, 10
         )
 
-        self.ser = serial.Serial('/dev/ttyAMA0', 115200)
+        self.bus = SMBus(1)
+        self.addr = 0x67
 
-        self.wheel_dist = 0.185  # m
-        self.counts_per_revolution = 480
-        self.max_counts_per_second = 900
-        self.wheel_radius = 0.04
+        self.wheel_dist = self.get_parameter("wheel_dist").value
+        self.counts_per_revolution = self.get_parameter("counts_per_revolution").value
+        self.wheel_radius = self.get_parameter("wheel_radius").value
+        self.max_counts_per_second = self.get_parameter("max_counts_per_second").value
+
         self.speed_mult = self.counts_per_revolution / (self.wheel_radius * 2 * math.pi)
 
-        self.get_logger().info('Twist subscriber node started!')
+        self.get_logger().info("Twist subscriber node started!")
 
-    def stop(self) -> None:
-        self.ser.write(self.START_FLAG + self.STOP_REQUEST)
+    def stop(self):
+        self.bus.write_i2c_block_data(self.addr, self.STOP_REQUEST)
 
-    def twist_callback(self, msg) -> None:
-        linear_x = int((msg.linear.x * self.speed_mult * 127) / self.max_counts_per_second)
-        linear_y = int((msg.linear.y * self.speed_mult * 127) / self.max_counts_per_second)
-        angular_z = int((msg.angular.z * self.speed_mult * 127) / self.max_counts_per_second)
+    def twist_callback(self, msg):
+        linear_x = int(
+            (msg.linear.x * self.speed_mult * 127) / self.max_counts_per_second
+        )
+        angular_z = int(
+            (msg.angular.z * self.speed_mult * 127) / self.max_counts_per_second
+        )
 
         def fitted(a):
             return min(max(a, -127), 127)
 
         linear_x = fitted(linear_x)
-        linear_y = fitted(linear_y)
         angular_z = fitted(angular_z)
 
-        linear_x_byte: bytes = int(linear_x).to_bytes(1, 'big', signed=True)
-        linear_y_byte: bytes = int(linear_y).to_bytes(1, 'big', signed=True)
-        angular_z_byte: bytes = int(angular_z).to_bytes(1, 'big', signed=True)
+        if angular_z < -60:
+            angular_z = -60
 
-        self.ser.write(self.START_FLAG + self.DRIVE_REQUEST + linear_x_byte +
-                       linear_y_byte + angular_z_byte)
+        linear_x_byte = int(linear_x) & 0xFF
+        angular_z_byte = int(angular_z) & 0xFF
 
-    def __del__(self) -> None:
+        self.get_logger().info(f"Bytes: {linear_x_byte}, {angular_z_byte}")
+
+        self.bus.write_i2c_block_data(
+            self.addr, self.DRIVE_REQUEST, [linear_x_byte, 0, angular_z_byte]
+        )
+
+    def __del__(self):
         self.stop()
 
 
-def main(args=None) -> None:
+def main(args=None):
     rclpy.init(args=args)
     twist_subscriber_node = TwistSubscriber()
     rclpy.spin(twist_subscriber_node)
@@ -66,5 +75,5 @@ def main(args=None) -> None:
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
