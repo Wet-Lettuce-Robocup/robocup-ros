be3db76f5ff25eedd9ca930b9ef6fdf754594c6b
William D'Olier <william@dolier.net>
2026-04-26 11:18:10 +1000

    changes to parameters

--- ros/robot_core/robot_core/servo_controller.py
+++ ros/robot_core/robot_core/servo_controller.py
@@ -1,18 +1,28 @@
 import math
 
-from smbus import SMBus
 import rclpy
 from rclpy.node import Node
+from smbus import SMBus
 from std_msgs.msg import Float32
 
 
 class ServoController(Node):
+    """Node providing topics to allow for servos to be controlled over i2c.
+
+    Attributes:
+        listen_topic (str): The topic subscribed to for angle commands. All angles are in radians.
+        servo_id (int): The ID of the servo on the STM32 to be controlled.
+        i2c_address (int): The I2C address of the STM32.
+        servo_cmd (int): The command sent to the STM32 to set servos.
+    """
+
     def __init__(self):
         super().__init__('servo_controller')
 
         self.declare_parameter('listen_topic', '/servo/default')
         self.declare_parameter('servo_id', 0)
         self.declare_parameter('i2c_address', 0x67)
+        self.declare_parameter('servo_cmd', 0x10)
 
         self.listen_topic: str = (
             self.get_parameter('listen_topic').get_parameter_value().string_value
@@ -21,7 +31,10 @@ class ServoController(Node):
             self.get_parameter('servo_id').get_parameter_value().integer_value & 0xFF
         )
         self.i2c_address: int = (
-            self.get_parameter('servo_id').get_parameter_value().integer_value & 0xFF
+            self.get_parameter('i2c_address').get_parameter_value().integer_value & 0xFF
+        )
+        self.servo_cmd: int = (
+            self.get_parameter('servo_cmd').get_parameter_value().integer_value & 0xFF
         )
 
         self.create_subscription(Float32, self.listen_topic, self.servo_callback, 10)
@@ -36,7 +49,9 @@ class ServoController(Node):
         degrees = int(self.rads_to_degrees(msg.data))
         degrees = min(max(degrees, 0), 180) & 0xFF
 
-        self.bus.write_i2c_block_data(self.i2c_address, self.servo_id, [degrees])
+        self.bus.write_i2c_block_data(
+            self.i2c_address, self.servo_cmd, [self.servo_id, degrees]
+        )
 
 
 def main(args=None):
