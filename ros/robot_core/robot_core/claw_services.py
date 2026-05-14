19b5fb028cbea5518534d64973140aacdbf742b4
TheRustedBeasty <bchangson@proton.me>
2026-05-14 21:13:41 +1000

    begin claw commands node

---
+++ ros/robot_core/robot_core/claw_services.py
@@ -0,0 +1,21 @@
+
+class ClawServices:
+    """
+    Node to handle claw commands, including:
+    - Open/close claw
+    - Lift claw up/down
+    - Open/close gate
+    """
+
+    LIFT_UP_ANGLE = 0
+    LIFT_DOWN_ANGLE = 90
+    CLAW_OPEN_ANGLE = 0
+    CLAW_CLOSED_ANGLE = 90
+    GATE_OPEN_ANGLE = 0
+    GATE_CLOSED_ANGLE = 90
+    
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
