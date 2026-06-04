fd1547ada2ad758aa1926f3d1db7f1df90141813
William D'Olier <56065795+will-da-pro@users.noreply.github.com>
2026-06-04 19:40:09 +1000

    Fixes (#30)
    * fan fixes
    
    * cameras and pwm fixes
    
    * oled by chatgpt
    
    * update oled with chatgpt again
    
    * trust its got this
    
    * start fan after idle
    
    * Update oled_controller.py
    
    * rotate oled
    
    * Update oled_controller.py
    
    * Update oled_controller.py
    
    * Update oled_controller.py
    
    * Update oled_controller.py
    
    * Update oled_controller.py
    
    * Update oled_controller.py
    
    * changes
    
    * hopefully fixed style errors
    
    * fixed test script and style errors
    
    * fixed oled function
    
    * more fixes for i2c stuff
    
    * removed unused time import
    
    ---------
    
    Co-authored-by: TheRustedBeasty <bchangson@proton.me>

--- ros/robot_core/robot_core/state_machine.py
+++ ros/robot_core/robot_core/state_machine.py
@@ -4,7 +4,7 @@ from gpiozero import OutputDevice
 from lifecycle_msgs.srv import ChangeState
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Int32
 
 
 class State(Enum):
@@ -41,6 +41,7 @@ class StateMachineNode(Node):
         self.idle_button_sub = self.create_subscription(
             Bool, '/idle_button', self.idle_button_callback, 10
         )
+        self.fan_pub = self.create_publisher(Int32, '/fan/target_speed', 10)
 
         # Lifecycle service clients
         self.line_follower_client = self.create_client(
@@ -71,6 +72,10 @@ class StateMachineNode(Node):
         if not msg.data:
             self.idle_toggle = not self.idle_toggle
 
+    def clean_exit(self):
+        self.en_3v3.off()
+        self.en_5v.off()
+
     def state_loop(self):
         from lifecycle_msgs.msg import Transition
 
@@ -80,16 +85,25 @@ class StateMachineNode(Node):
 
         elif self.current_state == State.IDLE:
             if not self.idle_toggle:
-                self.change_node_state(
-                    self.line_follower_client, Transition.TRANSITION_ACTIVATE
-                )
+                fan_msg = Int32()
+                fan_msg.data = 100
+                self.fan_pub.publish(fan_msg)
+
+                # self.change_node_state(
+                #     self.line_follower_client, Transition.TRANSITION_ACTIVATE
+                # )
+
                 self.current_state = State.LINE_FOLLOWING
 
         elif self.idle_toggle:
-            self.change_node_state(
-                self.line_follower_client, Transition.TRANSITION_DEACTIVATE
-            )
-            self.change_node_state(self.rescue_client, Transition.TRANSITION_DEACTIVATE)
+            fan_msg = Int32()
+            fan_msg.data = 0
+            self.fan_pub.publish(fan_msg)
+
+            # self.change_node_state(
+            #     self.line_follower_client, Transition.TRANSITION_DEACTIVATE
+            # )
+            # self.change_node_state(self.rescue_client, Transition.TRANSITION_DEACTIVATE)
             self.current_state = State.IDLE
 
         elif self.current_state == State.LINE_FOLLOWING:
@@ -123,6 +137,7 @@ def main(args=None):
     rclpy.init(args=args)
     node = StateMachineNode()
     rclpy.spin(node)
+    node.clean_exit()
     node.destroy_node()
     rclpy.shutdown()
 
