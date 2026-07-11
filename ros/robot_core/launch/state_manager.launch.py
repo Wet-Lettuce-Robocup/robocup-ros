679b70a64d3c1e3b9ea471167f551a3c7d55ecb4
BlueBM <100818726+BlueBM@users.noreply.github.com>
2026-07-11 21:02:41 +1000

    Merge auto fan speed into fan_controller node

--- ros/robot_core/launch/state_manager.launch.py
+++ ros/robot_core/launch/state_manager.launch.py
@@ -157,13 +157,6 @@ def generate_launch_description():
         output='screen',
         parameters=[config],
     )
-    fan_curve = Node(
-        package='robot_core',
-        executable='fan_curve',
-        name='fan_curve',
-        output='screen',
-        parameters=[config],
-    )
     front_led_controller = Node(
         package='robot_core',
         executable='front_led_controller',
